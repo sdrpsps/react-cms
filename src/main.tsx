514fcd35be044944291d94542e19e5c47e414fcb
sdrpsps <z826700@gmail.com>
2023-02-13 10:56:24 +0800

    fix: 🐛 修复了分页器使用英文的情况
    ## main
    - 启用了 antd 国际化
    - 修改默认语言为简体中文

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -6,6 +7,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </BrowserRouter>,
 );
