15f8c73a24ff3fad23f410e186c818f16058e110
sdrpsps <z826700@gmail.com>
2023-03-13 16:38:50 +0800

    fix: 🐛 vite 跨域

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,51 +1,55 @@
-import { login } from '@/api';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Form, Input, message } from 'antd';
-import { useCallback, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import {login} from '@/api';
+import {LockOutlined, UserOutlined} from '@ant-design/icons';
+import {Button, Form, Input, message} from 'antd';
+import {useCallback, useState} from 'react';
+import {useLocation, useNavigate} from 'react-router-dom';
 
 function Login() {
-  // 按钮的 loading 状态
-  const [btnLoading, setBtnLoading] = useState(false);
-  // 路由跳转
-  const navigate = useNavigate();
-  // 获取登录前的路由
-  const location = useLocation();
-  // 点击登录按钮触发登录
-  const onFinish = useCallback(async (values: any) => {
-    setBtnLoading(true);
-    try {
-      const res = await login(values);
-      localStorage.setItem('userInfo', JSON.stringify(res.data));
-      message.success('登录成功!');
-      navigate(location.state?.preURL || '/', { replace: true });
-    } finally {
-      setBtnLoading(false);
-    }
-  }, []);
+    // 按钮的 loading 状态
+    const [btnLoading, setBtnLoading] = useState(false);
+    // 路由跳转
+    const navigate = useNavigate();
+    // 获取登录前的路由
+    const location = useLocation();
+    // 点击登录按钮触发登录
+    const onFinish = useCallback(async (values: any) => {
+        setBtnLoading(true);
+        try {
+            const res = await login(values);
+            // 用户信息
+            localStorage.setItem('userInfo', JSON.stringify(res.data));
+            // 过期时间
+            localStorage.setItem('expirationTime', JSON.stringify(new Date().getTime()))
+            message.success('登录成功!');
+            navigate(location.state?.preURL || '/', {replace: true});
+        } finally {
+            setBtnLoading(false);
+        }
+    }, []);
 
-  return (
-    <div className="h-screen w-screen bg-login-background">
-      <Form
-        name="normal_login"
-        className="login-form w-96 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-        onFinish={onFinish}
-      >
-        <Form.Item name="username" rules={[{ required: true, message: '请填写用户名!' }]}>
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="admin" />
-        </Form.Item>
-        <Form.Item name="password" rules={[{ required: true, message: '请填写密码!' }]}>
-          <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="123456" />
-        </Form.Item>
+    return (
+        <div className="h-screen w-screen bg-login-background">
+            <Form
+                name="normal_login"
+                className="login-form w-96 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                onFinish={onFinish}
+            >
+                <Form.Item name="username" rules={[{required: true, message: '请填写用户名!'}]}>
+                    <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="admin"/>
+                </Form.Item>
+                <Form.Item name="password" rules={[{required: true, message: '请填写密码!'}]}>
+                    <Input prefix={<LockOutlined className="site-form-item-icon"/>} type="password"
+                           placeholder="123456"/>
+                </Form.Item>
 
-        <Form.Item className="text-center">
-          <Button type="primary" htmlType="submit" className="login-form-button" loading={btnLoading}>
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+                <Form.Item className="text-center">
+                    <Button type="primary" htmlType="submit" className="login-form-button" loading={btnLoading}>
+                        登录
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
 }
 
 export default Login;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +8,15 @@ export default defineConfig({
   resolve: {
     // 配置路径别名
     alias: {
-        '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
-},
-})
+  },
+  server: {
+    proxy: {
+      '/api/private/v1': {
+        target: 'http://72.18.83.161:3000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
